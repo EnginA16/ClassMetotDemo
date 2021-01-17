54b3bed4020cc841fe70c8fdd36a4ed38b0f4b1f
Engin Aydın <engin1637e@gmail.com>
2021-01-17 17:27:03 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekle(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi : " + customer.Ad + " " + customer.Soyad);
+        }
+
+        public void Listele(Customer customer)
+        {
+            Console.WriteLine("Müşteri listelendi : " + customer.Ad + " " + customer.Soyad);
+        }
+
+        public void Sil(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi : " + customer.Ad + " " + customer.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Ad = "Ahmet";
+            customer1.Soyad = "Yılmaz";
+            customer1.Id = 01;           
+
+            Customer customer2 = new Customer();
+            customer2.Ad = "Mehmet";
+            customer2.Soyad = "Öztürk";
+            customer2.Id = 02;
+
+            Customer customer3 = new Customer();
+            customer3.Ad = "Ayşe";
+            customer3.Soyad = "Demir";
+            customer3.Id = 03;           
+
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Ad : " + customer.Ad);
+                Console.WriteLine("Soyad : " + customer.Soyad);
+                Console.WriteLine("Id : " + customer.Id);
+                Console.WriteLine("--------------------");
+
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(customer1);
+            customerManager.Ekle(customer2);
+            customerManager.Ekle(customer3);
+            Console.WriteLine("--------------------");
+
+            customerManager.Listele(customer1);
+            customerManager.Listele(customer2);
+            customerManager.Listele(customer3);
+            Console.WriteLine("--------------------");
+
+            customerManager.Sil(customer1);
+            customerManager.Sil(customer2);
+            customerManager.Sil(customer3);
+
+        }
+    }
+}
